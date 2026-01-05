89a696dabbe499072b435a0012d8ff34aef08731
slchangtw <shunlung.chang@gmail.com>
2026-01-05 21:18:19 +0100

    feat(lambda-powertools-basics): add new module

---
+++ lambda-powertools-basics/terraform/iam.tf
@@ -0,0 +1,23 @@
+# IAM role for Lambda execution
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "lambda_execution_role_lambda_powertools_basics"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
