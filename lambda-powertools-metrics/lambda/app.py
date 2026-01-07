3a173fdb1fec4177ab7501c93672c4f439f7cc10
slchangtw <shunlung.chang@gmail.com>
2026-01-07 19:24:24 +0100

    feat: add module lambda-powertools-metrics

---
+++ lambda-powertools-metrics/lambda/app.py
@@ -0,0 +1,61 @@
+import json
+
+from aws_lambda_powertools import Logger, Metrics
+from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools.utilities.parser import ValidationError, parse
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from pydantic import BaseModel
+
+logger = Logger()
+metrics = Metrics()
+
+
+class OrderDetails(BaseModel):
+    order_id: str
+    item: str
+    amount: int
+    price: float
+
+
+@metrics.log_metrics
+def lambda_handler(event: OrderDetails, context: LambdaContext) -> dict:
+    # Log the incoming event
+    logger.info("Received event")
+    try:
+        event: OrderDetails = parse(model=OrderDetails, event=event)
+
+        # Calculate total cost
+        if event.amount <= 0 or event.price <= 0:
+            raise ValueError("Amount and price must be greater than 0")
+
+        total_cost = event.amount * event.price
+
+        logger.info(
+            f"Processing order {event.order_id}",
+            extra={"item": event.item, "total_cost": total_cost},
+        )
+
+        # Construct success response
+        response = {
+            "message": "Order processed successfully",
+            "order_id": event.order_id,
+            "total_cost": total_cost,
+            "status": "CONFIRMED",
+        }
+
+        metrics.add_metric(name="SuccessfulOrders", value=1, unit=MetricUnit.Count)
+
+        return {"statusCode": 200, "body": json.dumps(response)}
+
+    except ValidationError as e:
+        logger.exception("Validation error", extra={"event": event})
+        return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
+    except ValueError as e:
+        logger.exception("Value error", extra={"event": event})
+        return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
+    except Exception:
+        logger.exception("Internal server error")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Internal processing error"}),
+        }
---
+++ lambda-powertools-metrics/terraform/lambda.tf
@@ -0,0 +1,21 @@
+# Lambda function
+resource "aws_lambda_function" "lambda" {
+  function_name = "lambda-powertools-metrics"
+  role          = aws_iam_role.lambda_execution_role.arn
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:latest"
+  package_type  = "Image"
+  architectures = ["arm64"]
+
+  source_code_hash = null_resource.build_and_push_image.triggers.lambda_code_hash
+
+  timeout = 10
+
+ environment {   
+    variables = {
+      POWERTOOLS_SERVICE_NAME      = "order-service"
+      POWERTOOLS_METRICS_NAMESPACE = "order-service"
+    }
+  }
+
+  depends_on = [null_resource.build_and_push_image]
+}
