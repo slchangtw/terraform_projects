5f67d5549653bb1d7cc698fc46b23aa5b54596fe
slchangtw <shunlung.chang@gmail.com>
2026-01-10 20:16:29 +0100

    refactor: Replace hello-world-lambda with lambda-requests-example

--- hello-world-lambda/lambda/app.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": f"Hello world from {os.getenv('ENVIRONMENT')}!",
-    }
---
+++ lambda-requests-example/terraform/ecr.tf
@@ -0,0 +1,32 @@
+# Get ECR login token
+data "aws_ecr_authorization_token" "token" {}
+
+resource "aws_ecr_repository" "lambda_repository" {
+  name         = "lambda-requests-example"
+  force_delete = true
+}
+
+# Build and push Docker image
+resource "null_resource" "build_and_push_image" {
+  triggers = {
+    # Trigger rebuild when these files change
+    dockerfile_hash  = filemd5("../Dockerfile")
+    pyproject_hash   = filemd5("../pyproject.toml")
+    lambda_code_hash = join(",", [for f in fileset("../lambda", "**") : filemd5("../lambda/${f}")])
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Login to ECR
+      echo ${data.aws_ecr_authorization_token.token.password} | docker login --username AWS --password-stdin ${data.aws_ecr_authorization_token.token.proxy_endpoint}
+      
+      # Build the image
+      docker build --platform linux/arm64 -t ${aws_ecr_repository.lambda_repository.repository_url}:latest ..
+      
+      # Push the image
+      docker push ${aws_ecr_repository.lambda_repository.repository_url}:latest
+    EOT
+  }
+
+  depends_on = [aws_ecr_repository.lambda_repository]
+}
