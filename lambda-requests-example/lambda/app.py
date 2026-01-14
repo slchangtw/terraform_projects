15a9bdeef0522c6eeaf7bfeaf51bf46e2f1b4a26
slchangtw <shunlung.chang@gmail.com>
2026-01-14 19:18:11 +0100

    refactor: use content-hash for docker image tags and cleanup

--- lambda-requests-example/lambda/app.py
+++ lambda-requests-example/lambda/app.py
@@ -3,7 +3,6 @@ import requests
 
 def lambda_handler(event: dict, context: dict) -> dict:
     response = requests.get("https://api.github.com")
-    response.json()
     return {
         "statusCode": 200,
         "body": response.json(),
--- lambda-requests-example/terraform/lambda.tf
+++ lambda-requests-example/terraform/lambda.tf
@@ -1,14 +1,12 @@
-# Lambda function
 resource "aws_lambda_function" "lambda" {
   function_name = "lambda-requests-example"
   role          = aws_iam_role.lambda_execution_role.arn
-  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:${local.docker_image_tag}"
   package_type  = "Image"
   architectures = ["arm64"]
 
-  source_code_hash = null_resource.build_and_push_image.triggers.lambda_code_hash
-
   timeout = 10
+  memory_size = 128
 
   depends_on = [null_resource.build_and_push_image]
 }
